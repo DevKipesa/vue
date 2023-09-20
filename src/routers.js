987350ba2165b70b61cf17fdd3219efbcd0c7498
Dominic Kimani <kymdommie@gmail.com>
2023-09-20 11:42:49 +0300

    adding code

--- src/routers.js
+++ src/routers.js
@@ -1,16 +1,22 @@
 import Home from './components/Home.vue';
 import SignUp from './components/SignUp.vue';
-import {createRouter,createWebHashHistory} from 'vue-router';
+import { createRouter, createWebhHistory } from 'vue-router';
 
-const routes=[
+const routes = [
     {
-        name:'Home',
+        name: 'Home',
         component: Home,
-        path:'/'
+        path: '/'
     };
-    {
-        name:'SignUp',
+{
+    name: 'SignUp',
         component: SignUp,
-        path:'/sign-up'
-    }
+            path: '/sign-up'
+}
 ];
+
+const router= createRouter({
+    history: createWebhHistory(),
+    routes,
+});
+export default router;
