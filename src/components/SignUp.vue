a17cff27e6efc89f04b6ba24497752b333ccb1d5
Dominic Kimani <kymdommie@gmail.com>
2023-09-21 10:26:18 +0300

    adding a link to login

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -6,6 +6,9 @@
     <input type="text" v-model="email" placeholder="Enter Email" />
     <input type="password" v-model="password" placeholder="Enter Password" />
     <button v-on:click="signUp">Sign Up</button>
+    <p>
+      <router-link to="/login">Login</router-link>
+    </p>
 </div>
 </template>
 <script>
