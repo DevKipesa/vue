28df80097ce8efc8476521b1579b28313370c08a
Dominic Kimani <kymdommie@gmail.com>
2023-10-10 11:00:28 +0300

    user name on homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,22 @@
 <template>
 <HeaderBar />
-    <h1>Hello, Welcome to Homepage Peeps</h1>
+    <h1>Hello {{name}}, Welcome to Homepage Peeps</h1>
 </template>
 <script>
 import HeaderBar from './Header.vue'
 export default {
     name: 'HomePage',
+    data (){
+      return{
+        name:''
+      }
+    },
     components:{
         HeaderBar
     },
     mounted(){
      let user=localStorage.getItem('user-info');
+     this.name=JSON.parse(user).name;
      if(!user){
       this.$router.push({name:'SignUp'})
      }
