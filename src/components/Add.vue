fa701093f61a0d1ba0f9441f972d56e2bb3d7c92
Dominic Kimani <kymdommie@gmail.com>
2023-10-10 10:23:46 +0300

    add page

---
+++ src/components/Add.vue
@@ -0,0 +1,19 @@
+<template>
+<HeaderBar />
+    <h1>Hello, Welcome to Addpage Peeps</h1>
+</template>
+<script>
+import HeaderBar from './Header.vue'
+export default {
+    name: 'AddPage',
+    components:{
+        HeaderBar
+    },
+    mounted(){
+     let user=localStorage.getItem('user-info');
+     if(!user){
+      this.$router.push({name:'SignUp'})
+     }
+ }
+}
+</script>
---
+++ src/components/Home copy.vue
@@ -0,0 +1,19 @@
+<template>
+<HeaderBar />
+    <h1>Hello, Welcome to Homepage Peeps</h1>
+</template>
+<script>
+import HeaderBar from './Header.vue'
+export default {
+    name: 'HomePage',
+    components:{
+        HeaderBar
+    },
+    mounted(){
+     let user=localStorage.getItem('user-info');
+     if(!user){
+      this.$router.push({name:'SignUp'})
+     }
+ }
+}
+</script>
